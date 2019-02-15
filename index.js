a3ecf682e4b8cc334a0ba042cec12d63a17f00ac
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-15 22:37:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function increaseRankBy(n) {
   }
 }
 function deepestChild() {
-  return document.querySelector('div#grand-node').querySelectorAll('div');
+  return document.querySelector('div#grand-node').querySelectorAll('div').innerHTML;
 }
