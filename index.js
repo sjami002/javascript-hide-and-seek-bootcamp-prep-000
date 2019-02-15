0ebe4eac2d0a803f5fa98cefd1121a389f9e620c
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-15 22:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 function nestedTarget() {
-  const nest = document.querySelector('div#nested ')
+  const nest = document.querySelector('div#nested div.target');
 }
