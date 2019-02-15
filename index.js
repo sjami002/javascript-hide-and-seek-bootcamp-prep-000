b0ff8181650f019a7a91748a9cffbab02679ed59
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-15 22:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function nestedTarget() {
   return document.querySelector('div#nested div.target');
 }
 function increaseRankBy(n) {
-  
+  var rank = document.querySelector('ul.ranked-list li')
+  rank = parseInt(rank) + n
 }
