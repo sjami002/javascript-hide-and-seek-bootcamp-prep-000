49617116d6b4009c4c3e979cdb9381cdeac33658
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-15 22:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function nestedTarget() {
   return document.querySelector('div#nested div.target');
 }
 function increaseRankBy(n) {
-  var rank = document.querySelectorAll('ul.ranked-list li').innerHTML;
-  return parseInt(rank + n)
+  var rank = document.querySelectorAll('ul.ranked-list li');
+  for(i=0; i<rank.length; i++) {
+
+  }
 }
